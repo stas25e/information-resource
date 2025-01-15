1e2bfa81c772795284ddebd97f28f98213d1a686
stas25e <stas25e@gmail.com>
2025-01-15 23:07:34 +0300

    loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -30,6 +30,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
     const cssLoader = buildCssLoader(isDev);
 
+    // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
